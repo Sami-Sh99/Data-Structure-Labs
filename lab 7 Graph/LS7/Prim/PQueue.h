5d9be592510d2e9beba19b3ec6fad1a10caf2096
Sami-Sh99 <47642506+Sami-Sh99@users.noreply.github.com>
2019-03-24 06:14:56 +0200

    Delete PQueue.h

--- lab 7 Graph/LS7/Prim/PQueue.h
+++
@@ -1,7 +0,0 @@
-#include "Type_PQueue.h"
-extern pqueue CreateQueue();
-extern int EnQueue(pqueue *q, element e, int priority);
-extern int DeQueue(pqueue *q);
-extern int Front(pqueue q, element *e);
-extern int isEmptyQueue(pqueue q);
-extern void update_priority(pqueue*, element, int);
